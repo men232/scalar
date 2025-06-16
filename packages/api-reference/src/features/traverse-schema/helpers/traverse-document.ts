c295e305aa1c2d8c9bf7d09457dd1e905f9df992
Andrew L <andrew.io.dev@gmail.com>
2025-06-16 21:56:49 +0200

    fix(api-reference): hide models section if "models" tag exists in x-tagGroups

--- packages/api-reference/src/features/traverse-schema/helpers/traverse-document.ts
+++ packages/api-reference/src/features/traverse-schema/helpers/traverse-document.ts
@@ -1,11 +1,11 @@
 import type { OpenAPIV3_1 } from '@scalar/openapi-types'
 
+import type { TagsMap, TraverseSpecOptions, TraversedEntry } from '@/features/traverse-schema/types'
 import { traverseDescription } from './traverse-description'
 import { traversePaths } from './traverse-paths'
 import { traverseSchemas } from './traverse-schemas'
 import { traverseTags } from './traverse-tags'
 import { traverseWebhooks } from './traverse-webhooks'
-import type { TagsMap, TraverseSpecOptions, TraversedEntry } from '@/features/traverse-schema/types'
 
 /**
  * Travers the OpenAPI Document and ensure we only do it once
@@ -27,15 +27,38 @@ export const traverseDocument = (
   const tagsMap: TagsMap = new Map(
     document.tags?.map((tag: OpenAPIV3_1.TagObject) => [tag.name ?? 'Untitled Tag', { tag, entries: [] }]) ?? [],
   )
+
+  const entries: TraversedEntry[] = []
+
+  // Add models if they are not hidden
+  if (!config.value.hideModels && document.components?.schemas) {
+    const untaggedModels = traverseSchemas(document, tagsMap, titles, getModelId)
+
+    tagsMap.set('models', {
+      tag: {
+        id: getModelId(),
+        title: 'Models',
+        name: 'Models',
+        children: [],
+      },
+      entries: untaggedModels,
+    })
+  }
+
   traversePaths(document, tagsMap, titles, getOperationId)
 
-  const entries: TraversedEntry[] = traverseDescription(document.info?.description, titles, getHeadingId)
+  entries.push(...traverseDescription(document.info?.description, titles, getHeadingId))
+
   const untaggedWebhooks = traverseWebhooks(document, tagsMap, titles, getWebhookId)
   const tagsEntries = traverseTags(document, tagsMap, titles, {
     getTagId,
     tagsSorter: config.value.tagsSorter,
     operationsSorter: config.value.operationsSorter,
   })
+
+  const modelTagUsed = tagsEntries
+    .flatMap((v) => ('tag' in v ? (Array.isArray(v.tag?.tags) ? v.tag?.tags : []) : []))
+    .includes('models')
 
   // Add tagged operations, webhooks and tagGroups
   entries.push(...tagsEntries)
@@ -50,17 +73,12 @@ export const traverseDocument = (
     })
   }
 
-  // Add models if they are not hidden
-  if (!config.value.hideModels && document.components?.schemas) {
-    const models = traverseSchemas(document, tagsMap, titles, getModelId)
-
-    if (models.length) {
-      entries.push({
-        id: getModelId(),
-        title: 'Models',
-        children: models,
-      })
-    }
+  if (!modelTagUsed && tagsMap.has('models')) {
+    entries.push({
+      id: getModelId(),
+      title: 'Models',
+      children: tagsMap.get('models')!.entries,
+    })
   }
 
   return { entries, titles }
